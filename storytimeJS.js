a357093f2faa7b09572fe8d23a798494a3f72797
Richard Huang <63317612+CheeezAir@users.noreply.github.com>
2020-07-03 13:56:02 -0700

    console log hash

--- storytimeJS.js
+++ storytimeJS.js
@@ -10,6 +10,8 @@ async function digestMessage(message) {
 }
 async function checkHash(){
     const digestHex = await digestMessage(text);
+    console.log(digestHex);
+
     if(digestHex === "0983ea1ecee4c628edca1b44c3c719ddd1eec3c0babd11a65eadd43ba336b730"){
         document.location.href = "private.html";
 
